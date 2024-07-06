b8a1c6e360fcee49a7815c27532eac7953561aaf
Alvin Francis <nivlafx@gmail.com>
2024-07-06 11:51:10 -0400

    pihooks: Set api level to 32
      * An api level lower that 32 is not required as software attestation can be done with it and lower, 33 and above can only be hardware attestation.
    
     * Remove the previous security patch, not required.
    
    Change-Id: Ibff183584586f4bc854e4bbff1da823a332331ba
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- build/core/pihooks.mk
+++ build/core/pihooks.mk
@@ -1,6 +1,5 @@
 # RisingOS
 
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    persist.sys.pihooks.security_patch=2017-12-05 \
-    persist.sys.pihooks.first_api_level=24
+    persist.sys.pihooks.first_api_level=32
 
